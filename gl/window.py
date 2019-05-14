2530d60c66611b5644eebb3ce64dfabb086df0dd
unknown <julwi727@student.liu.se>
2019-05-14 23:20:45 +0200

    Add support for component ids and text boxes

--- gl/window.py
+++ gl/window.py
@@ -1,6 +1,8 @@
 import pygame
 import gl.button as glb
+import gl.text_box as gltb
 import infrastructure.command as cmd
+import gui_id as gui
 from pygame.locals import *
  
 class Window:
@@ -10,6 +12,7 @@ class Window:
         self._display_surf = None
         self.size = self.width, self.height = width, height
         self.buttons = []
+        self.text_boxes = []
         self.selected_button = None
         self.message_queue = cmd.Command()
  
@@ -34,15 +37,24 @@ class Window:
         if self.message_queue.has_message():
             command = self.message_queue.get_message()
             
-            if command == cmd.CommandType.MOVE:
+            if command[0] == cmd.CommandType.MOVE:
                 print("Move")
-            if command == cmd.CommandType.SELECT_PIECE:
+            if command[0] == cmd.CommandType.SELECT_PIECE:
                 print("Select")
+            if command[0] == cmd.CommandType.CLEAR_TEXT:
+                self.clear_text(command[1])
+            if command[0] == cmd.CommandType.ADD_ROW:
+                self.add_row(command[1])
+
         
     def on_render(self):
         self._display_surf.fill(self.fill_color)
         for button in self.buttons:
             button.draw(self._display_surf)
+        
+        for text_box in self.text_boxes:
+            text_box.draw(self._display_surf)
+
         pygame.display.update()
 
     def on_cleanup(self):
@@ -59,9 +71,12 @@ class Window:
             self.on_render()
         self.on_cleanup()
 
-    def add_button(self, color, x, y, width, height, text='', command=''):
-        self.buttons.append(glb.Button(color, x, y, width, height, text, command))
+    def add_button(self, color, x, y, width, height, component_id, text='', command=''):
+        self.buttons.append(glb.Button(color, x, y, width, height, component_id, text, command))
     
+    def add_text_box(self, color, x, y, width, height, component_id):
+        self.text_boxes.append(gltb.TextBox(color, x, y, width, height, component_id))
+
     def check_clicks(self, pos):
         for button in self.buttons:
             if button.hit(pos):
@@ -71,9 +86,21 @@ class Window:
     def check_command(self, pos):
         for button in self.buttons:
             if button.hit(pos):
-                self.message_queue.add_message(button.command)
+                self.message_queue.add_message(button.command, button.component_id)
 
     def deselect_last(self):
         if self.selected_button != None:
             self.selected_button.toggle_select()
             self.selected_button = None
+
+    def clear_text(self, component_id):
+        print("Clear text_box with id: " + str(component_id))
+        for text_box in self.text_boxes:
+            if text_box.component_id == component_id:
+                text_box.clear()
+
+    def add_row(self, component_id):
+        print("Add row to text_box with id: " + str(component_id))
+        for text_box in self.text_boxes:
+            if text_box.component_id == component_id:
+                text_box.add_row("Hej")
